eadc4f519816763160e56e4555c18d94d8700be3
Gaara <30978454+Linxbb@users.noreply.github.com>
2018-01-22 10:56:35 +0800

    Update and rename Golang Log to GolangLog.go
    const (
    	Ldate         = 1 << iota     //日期示例： 2009/01/23
    	Ltime                         //时间示例: 01:23:23
    	Lmicroseconds                 //毫秒示例: 01:23:23.123123.
    	Llongfile                     //绝对路径和行号: /a/b/c/d.go:23
    	Lshortfile                    //文件和行号: d.go:23.
    	LUTC                          //日期时间转为0时区的
    	LstdFlags     = Ldate | Ltime //Go提供的标准抬头信息
    )

--- GolangLog.go
+++ GolangLog.go
@@ -1,16 +1,5 @@
-log.SetFlags(log.Llongfile)
-使log输出加上行数
-
-const (
-	Ldate         = 1 << iota     //日期示例： 2009/01/23
-	Ltime                         //时间示例: 01:23:23
-	Lmicroseconds                 //毫秒示例: 01:23:23.123123.
-	Llongfile                     //绝对路径和行号: /a/b/c/d.go:23
-	Lshortfile                    //文件和行号: d.go:23.
-	LUTC                          //日期时间转为0时区的
-	LstdFlags     = Ldate | Ltime //Go提供的标准抬头信息
-)
-
+//log.SetFlags(log.Llongfile)
+//使log输出加上行数
 
 import(
   "github.com/gocraft/dbr"
@@ -19,7 +8,7 @@ import(
 	"io"
 )
 
-var 	Warning,Error * log.Logger
+var Warning,Error * log.Logger
 
 func init() {
 	errFile,err:=os.OpenFile("errors.log",os.O_CREATE|os.O_WRONLY|os.O_APPEND,0666)
