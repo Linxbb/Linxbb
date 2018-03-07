d83cca49d0f6af0667aac8548d857d888a14b8d4
Holter <30978454+codingObsession@users.noreply.github.com>
2018-03-07 10:54:54 +0800

    Create Golang http.go

---
+++ Golang http.go
@@ -0,0 +1,27 @@
+package main  
+  
+import (  
+        "fmt"  
+        "net/url"  
+        "net/http"  
+        "io/ioutil"  
+        "log"  
+)  
+  
+func main() {  
+        u, _ := url.Parse("http://localhost:8001/")  //todo head  
+        q := u.Query()  
+        q.Set("username", "user")  
+        q.Set("password", "passwd")  
+        u.RawQuery = q.Encode()  
+        res, err := http.Get(u.String());  
+        if err != nil {   
+              log.Fatal(err) return   
+        }  
+        result, err := ioutil.ReadAll(res.Body)   
+        res.Body.Close()   
+        if err != nil {   
+              log.Fatal(err) return   
+        }   
+        fmt.Printf("%s", result)  
+}
